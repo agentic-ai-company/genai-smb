52c355291ad30d811de5e9280d944d9cc1292f10
Sivaraj Sankar <98452182+Sivaraj-Sankar@users.noreply.github.com>
2026-05-09 22:25:27 +0530

    Update config.py

--- src/app/config.py
+++ src/app/config.py
@@ -1,6 +1,7 @@
 # src/app/config.py
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+
 class Settings(BaseSettings):
     app_name: str = "FastAPI Hello"
     environment: str = "development"
