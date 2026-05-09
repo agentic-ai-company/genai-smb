ac018e620732f18a1c6133b730aa09a898677c23
Sivaraj Sankar <98452182+Sivaraj-Sankar@users.noreply.github.com>
2026-05-09 22:28:05 +0530

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,13 +1,16 @@
 # tests/test_main.py
 from fastapi.testclient import TestClient
+
 from src.app.main import app
 
 client = TestClient(app)
+
 
 def test_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json()["message"] == "Hello, World!"
+
 
 def test_health():
     response = client.get("/health")
